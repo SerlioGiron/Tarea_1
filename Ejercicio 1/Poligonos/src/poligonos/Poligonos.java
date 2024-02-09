ee413e7f78a0fd41d23ea07a11cdfc3338e02146
Daniel <danielisaacjuarez@gmail.com>
2024-02-08 23:12:12 -0600

    push pols java

--- Ejercicio 1/Poligonos/src/poligonos/Poligonos.java
+++ Ejercicio 1/Poligonos/src/poligonos/Poligonos.java
@@ -18,9 +18,9 @@ public class Poligonos {
      */
     public static void main(String[] args) {
         
-//        System.out.println("args[0]" + args[0]);
-//        System.out.println("args[1]" + args[1]);
-//        System.out.println("args[2]" + args[2]);
+        System.out.println("args[0]" + args[0]);
+        System.out.println("args[1]" + args[1]);
+        
         // Create a Random object
         Random random = new Random();
 
