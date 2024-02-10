bc0623ee8e44a3c74991c294940fef1bc94040a9
Daniel <danielisaacjuarez@gmail.com>
2024-02-09 22:48:54 -0600

    Timestamps en Poligonos Pyhton Agregado

--- Ejercicio 1/python_poligonos/poligonos.py
+++ Ejercicio 1/python_poligonos/poligonos.py
@@ -1,19 +1,14 @@
 import random
 import math
 import sys
+import time
 
-num_arguments = len(sys.argv)
-print("Number of arguments:", num_arguments)
-print("argv[0]: ",sys.argv[0] )
-print("argv[1]: ",sys.argv[1] )
-print("argv[2]: ",sys.argv[2] )
+inicioEjecucion = time.time() #Inicio de Ejecucion
 
-random.seed()
-
+random.seed() #Variables para Generar los Angulos
 radius = 200
 puntos = int(sys.argv[2])
 angulo = 360 / puntos
-
 cont = 0
 angulos = []
 
@@ -26,62 +21,60 @@ totalperimetros = 0
 veces = int(sys.argv[1])
 distanciaPerimetros = []
 
-for _ in range(veces):
-    print("\n")
+#Generacion de Poligonos y Calculo de Perimetros
+totalGeneracionPoligonos = 0;
+totalCalculoPerimetros = 0;
+
+for j in range(veces):
     pointsx = []
     pointsy = []
+    #Generacion de Poligonos
+    inicioGeneracionPoligonos = time.time()
     for angle in angulos:
-        print("angulo:", angle)
         randomNumber_alterar = 0.01 + random.random() * 0.99
         x = int(randomNumber_alterar * radius * math.cos(math.radians(angle)))
         y = int(randomNumber_alterar * radius * math.sin(math.radians(angle)))
-        print("x:", x)
-        print("y:", y)
-        # x += 200
-        # y += 200
         pointsx.append(x)
         pointsy.append(y)
-        # aca hace los puntos
+    finGeneracionPoligonos = time.time()
+    totalGeneracionPoligonos += (finGeneracionPoligonos - inicioGeneracionPoligonos)
 
-
-
+    #Calculo de Perimetros
+    inicioCalculoPerimetros = time.time()
     sumPerimetros = 0
     for i in range(len(pointsx)):
         xUno, xDos, yUno, yDos, xCoordinate, yCoordinate, xSquared, ySquared, insideSum, squareRoot = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-        
         xUno = pointsx[i]
         yUno = pointsy[i]
-        
         if i == len(pointsx) - 1:
             xDos = pointsx[0]
             yDos = pointsy[0]
         else:
             xDos = pointsx[i + 1]
             yDos = pointsy[i + 1]
-        
-        print("Coordenadas {}: ({}, {}) and ({}, {})".format(i, xUno, yUno, xDos, yDos))
-        
+
+        print("Coordenadas {}: ({}, {}) and ({}, {})".format(i, xUno, yUno, xDos, yDos))        
         xCoordinate = xDos - xUno
         yCoordinate = yDos - yUno
-        
         xSquared = xCoordinate * xCoordinate
         ySquared = yCoordinate * yCoordinate
-        
         insideSum = xSquared + ySquared
-        
         squareRoot = math.sqrt(insideSum)
         distanciaPerimetros.append(squareRoot)
         print("Distancia {}: {}".format(i, squareRoot))
+    finCalculoPerimetros = time.time()
+    totalCalculoPerimetros += (finCalculoPerimetros - inicioCalculoPerimetros)
+    print("Fin del Poligono ", j+1, "\n")
 
-        
-    sumPerimetros = sum(distanciaPerimetros)
-    #esto es el perimtetro del poligono
-    print("suma de todos: ", sumPerimetros)
-    print(' ---- fin del poligono ---- ')
-
-
-
-
+sumPerimetros = sum(distanciaPerimetros)
 avgPerimetros = sumPerimetros / len(distanciaPerimetros)
 
-print("El Promedio de los Perimetros Es:", avgPerimetros)
+print("Suma Total: ", sumPerimetros)
+print("El Promedio de los Perimetros Es:", avgPerimetros, "\n")
+
+finEjecucion = time.time()
+totalEjecucion = (finEjecucion - inicioEjecucion)
+
+print("Tiempo de Generacion de Poligonos: " , (totalGeneracionPoligonos*1000) , " milisegundos")
+print("Tiempo de Calculo de Todos los Perimetros: " , (totalCalculoPerimetros*1000) , " milisegundos")
+print("Tiempo Total de Ejecucion: " , (totalEjecucion*1000) , " milisegundos")
